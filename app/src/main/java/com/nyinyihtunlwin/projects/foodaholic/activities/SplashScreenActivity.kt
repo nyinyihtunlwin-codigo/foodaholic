d596cbf6788b8f3456a6eebb5f4c3492dd889153
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2019-01-18 17:00:29 +0630

    Splash Screen ui updated.

--- app/src/main/java/com/nyinyihtunlwin/projects/foodaholic/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/nyinyihtunlwin/projects/foodaholic/activities/SplashScreenActivity.kt
@@ -3,7 +3,6 @@ package com.nyinyihtunlwin.projects.foodaholic.activities
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import com.nyinyihtunlwin.projects.foodaholic.R
 
 
@@ -11,8 +10,6 @@ class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        actionBar?.hide()
         setContentView(R.layout.activity_splash_screen)
         Handler().postDelayed({
             startActivity(MainActivity.newInstnace(applicationContext))
